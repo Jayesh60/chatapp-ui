c5a6980e40528444d601b7019f6a96a70a521f0f
Jayesh Wadhe <jwadhe46@gmail.com>
2023-12-03 22:49:46 +0530

    ui

--- app/components/MessageContainer.jsx
+++ app/components/MessageContainer.jsx
@@ -4,20 +4,20 @@ const MessageContainer = () => {
 
   return (
     <div className="h-full w-full flex mt-auto px-4 py-2 flex-col gap-1 bg-gray overflow-y-scroll no-scrollbar">
-      <Message Message={"Hello 👏"}/>
+      <Message Message={"Hello 👏"} img={'/common/jayesh.svg'}/>
       <Message  owner={true} Message={"Hello, there"}/>
       <Message Message={"How are you?"}/>
       <Message Message={"I'm Fine, mate!"} owner/>
-      <Message Message={"How are you?"}/>
-      <Message Message={"How are you?"} sameSender/>
-      <Message Message={"How are you?"} sameSender/>
-      <Message Message={"How are you?"} sameSender/>
-      <Message Message={"How are you?"} sameSender/>
-      <Message Message={"How are you?"} sameSender/>
-      <Message Message={"How are you?"} sameSender/>
-      <Message Message={"How are you?"} sameSender/>
-      <Message Message={"How are you?"} sameSender/>
-      <Message Message={"How are you?"} sameSender/>
+      <Message Message={"How are you?"} img={'/common/jayesh.svg'}/>
+      <Message Message={"How are you?"} sameSender img={'/common/jayesh.svg'}/>
+      <Message Message={"How are you?"} sameSender img={'/common/jayesh.svg'}/>
+      <Message Message={"How are you?"} sameSender img={'/common/jayesh.svg'}/>
+      <Message Message={"How are you?"} sameSender img={'/common/jayesh.svg'}/>
+      <Message Message={"How are you?"} sameSender img={'/common/jayesh.svg'}/>
+      <Message Message={"How are you?"} sameSender img={'/common/jayesh.svg'}/>
+      <Message Message={"How are you?"} sameSender img={'/common/jayesh.svg'}/>
+      <Message Message={"How are you?"} sameSender img={'/common/jayesh.svg'}/>
+      <Message Message={"How are you?"} sameSender img={'/common/jayesh.svg'}/>
       <Message Message={"Dont spam Bro!"} owner/>
       <Message Message={"Whats your problem??"} owner sameSender/>
     </div>
